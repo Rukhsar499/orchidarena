508ec9f1d506f7b864bde81614de6fad95eae2b6
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-09-26 17:43:11 +0530

    hii

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Image from "next/image";
+
 import OrchidArena from "../components/OrchidArena"
 // import CounterSection from "../components/CounterSection"
 // import TestimonialSlider from "../components/TestimonialSlider"
